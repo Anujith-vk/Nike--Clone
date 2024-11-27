c542212a65a165954f3402b7539be6aa6d998ea6
Anujith Vk <158153849+Anujith-vk@users.noreply.github.com>
2024-11-27 20:02:24 +0530

    Update middleware.js

--- backend/Middleware/middleware.js
+++ backend/Middleware/middleware.js
@@ -3,9 +3,6 @@ const jwt = require('jsonwebtoken');
 const isauthenticated = async function (req, res, next) {
     const auth = req.headers.authorization;
     try {
-        if (!auth) {
-            return res.status(400).json({ message: "Not authorized" });
-        }
         const token = auth.split(" ")[1]; 
         jwt.verify(token, process.env.jwt_key, (error, decoded) => {
             if (error) {
