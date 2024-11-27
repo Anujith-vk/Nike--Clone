bd73b162dd80565381ccf24205275ed4f6b953f1
Anujith Vk <158153849+Anujith-vk@users.noreply.github.com>
2024-11-27 20:13:37 +0530

    Update middleware.js

--- backend/Middleware/middleware.js
+++ backend/Middleware/middleware.js
@@ -3,14 +3,20 @@ const jwt = require('jsonwebtoken');
 const isauthenticated = async function (req, res, next) {
     const auth = req.headers.authorization;
     try {
-        const token = auth.split(" ")[1]; 
-        jwt.verify(token, process.env.jwt_key, (error, decoded) => {
-            if (error) {
-                return res.status(400).json({ message: "Please Login First" });
-            }
-            req.user = decoded; 
-            next();  
-        });
+        if (!auth) {
+            return res.status(400).json({ message: "Not authorized" });
+        }
+        else{
+            const token = auth.split(" ")[1]; 
+            jwt.verify(token, process.env.jwt_key, (error, decoded) => {
+                if (error) {
+                    return res.status(400).json({ message: "Please Login First" });
+                }
+                req.user = decoded; 
+                next();  
+            });
+        }
+       
     } catch (error) {
         return res.status(500).json({ message: "Unexpected error occurred", error: error.message });
     }
